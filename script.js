af5bd92adeafb4c08880a68dcd5f53c5ca37b908
ChnTionR <cnrkaya008@gmail.com>
2025-05-04 01:53:34 +0300

    Update script.js
    fixed again

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener('mousemove', (mouse) => {
     }
 
     if(Math.abs(L1length - L2length) > d){
-        d = 0
+        d = Math.abs(L1length - L2length);
     }
     
     drawBoard();
